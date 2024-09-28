50a357ef99cc509bfda21dbac11cc39c224b9d69
samwatsonUU <Watson-S28@ulster.ac.uk>
2024-09-28 18:34:57 +0100

    Add project files.

---
+++ Calculator+Try&Catch/Program.cs
@@ -0,0 +1,72 @@
+﻿
+CalculatorApp();
+
+void CalculatorApp()
+{
+
+    try
+    {
+        // Prompt the user to enter the first number
+        Console.Write("Enter the first number: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Prompt the user to enter the second number
+        Console.Write("Enter the second number: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Prompt the user to enter the operation
+        Console.Write("Enter the operation (+, -, *, /): ");
+
+        // Keyboard input is by default of type 'string'
+        // and requires conversion to 'char' to work
+        // with the switch statement
+        char operation = Convert.ToChar(Console.ReadLine());
+        int result = 0;
+
+        // Perform the operation
+        switch (operation)
+        {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                Console.WriteLine("Invalid operation.");
+                return;
+        }
+        // Output the result to the user
+        Console.WriteLine($"Result: {result}");
+
+
+    }
+    catch (FormatException ex)
+    {
+
+        // Handle the case where the input is not valid
+        Console.WriteLine("Please enter a valid number");
+
+    }
+    catch (DivideByZeroException ex)
+    {
+
+        // Handle the divide by zero error
+        Console.WriteLine($"You cannot divide by zero.");
+
+    }
+    finally
+    {
+
+        // This block runs no matter what
+        Console.WriteLine("Operation completed.");
+
+    }
+
+}
